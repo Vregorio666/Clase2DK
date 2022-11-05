c019b50aadbc0919dc84cd6e3fe671166c92d85f
Vregorio666 <mgrevergara@gmail.com>
2022-11-05 12:50:15 -0300

    Codigo Inicial

---
+++ Clase2.R
@@ -0,0 +1,20 @@
+## INVESTIGACION REPRODUCIBLE
+#La investigación reproducible (IR) no es lo mismo que la investigación replicable
+#La IR implica que desde los mismos datos y/o el mismo código 
+#se generaránlos mismos resultados.
+
+# PUBLICACION + CODIGO + DATOS = REPRODUCCION/REPLICA COMPLETA
+library(tidyverse)
+library(knitr)
+library(kableExtra)
+
+# 3 pasos para crear un REPOSITORIO
+#Git add: Agregar los archivos que va a guardar al repositorio
+# Add con la A en pestaña GI
+
+#Git commit: Guardar en repositorio local ( MI PC)
+# Sirve para ver versiones anteriores
+# Se guarda señalando un mensaje claro y relevante sobre el cambio
+
+
+#Git push : Guardar en la nube
